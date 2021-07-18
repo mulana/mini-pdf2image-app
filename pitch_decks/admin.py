9dbc5b4c3b370323f4733f2904d3c2c72e115fc7
mulana <mulana82@gmail.com>
2021-07-18 12:05:27 -0700

    Initial commit with 2 models

---
+++ pitch_decks/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from .models import PitchDeck, DeckImage
+
+admin.site.register(PitchDeck)
+admin.site.register(DeckImage)
---
+++ pitch_decks/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class PitchDeck(models.Model):
+    title = models.TextField()
+    presentation_file = models.FileField(upload_to='decks/')
+
+    def __str__(self):
+        return self.title
+
+
+class DeckImage(models.Model):
+    pitch_deck = models.ForeignKey(PitchDeck, on_delete=models.CASCADE)
+    title1 = models.TextField()
+    slide = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.title1
