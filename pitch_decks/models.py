b53de54090d87fc906c4e6a9175e87050b10eea4
mulana <mulana82@gmail.com>
2021-07-18 16:49:03 -0700

    Change text field to charfields

--- pitch_decks/models.py
+++ pitch_decks/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class PitchDeck(models.Model):
-    title = models.TextField()
+    title = models.CharField(max_length=64)
     presentation_file = models.FileField(upload_to='decks/')
 
     def __str__(self):
@@ -11,7 +11,7 @@ class PitchDeck(models.Model):
 
 class DeckImage(models.Model):
     pitch_deck = models.ForeignKey(PitchDeck, on_delete=models.CASCADE, related_name="deck_images")
-    title = models.TextField()
+    title = models.CharField(max_length=64)
     slide = models.ImageField(upload_to='images/')
 
     def __str__(self):
