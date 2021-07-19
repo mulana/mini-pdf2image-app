2e803cb1df5008d4380a2ae86426bc876a012c6b
mulana <mulana82@gmail.com>
2021-07-19 07:27:43 -0700

    Connect PitchDeck to user

--- pitch_decks/models.py
+++ pitch_decks/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class PitchDeck(models.Model):
     title = models.CharField(max_length=64)
     presentation_file = models.FileField(upload_to='decks/')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
--- pitch_decks/views.py
+++ pitch_decks/views.py
@@ -4,6 +4,7 @@ from django.urls import reverse_lazy
 
 from .forms import PitchDeckForm
 from .models import PitchDeck
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 # We should probably have some pagination here.
@@ -11,15 +12,29 @@ class HomePageView(ListView):
     model = PitchDeck
     template_name = 'home.html'
 
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get the context
+        context = super(HomePageView, self).get_context_data(**kwargs)
+        if not self.request.user.is_authenticated:
+            context['decks'] = []
+            return context
+        # Create any data and add it to the context
+        context['decks'] = PitchDeck.objects.filter(user=self.request.user)
+        return context
 
-class CreatePitchDeckView(CreateView):
+
+class CreatePitchDeckView(LoginRequiredMixin, CreateView):
     model = PitchDeck
     form_class = PitchDeckForm
     template_name = 'upload_deck.html'
     success_url = reverse_lazy('home')
 
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
 
-class DeletePitchDeckView(DeleteView):
+
+class DeletePitchDeckView(LoginRequiredMixin, DeleteView):
     model = PitchDeck
 
     def delete(self, request, *args, **kwargs):
